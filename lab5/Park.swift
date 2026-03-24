5f47f420de54907616adbca5aba05f4f6a8d851b
Yuvam <yuvam05.ed@gmail.com>
2026-03-23 20:50:12 -0700

    initialCommit

---
+++ lab5/Park.swift
@@ -0,0 +1,20 @@
+//
+//  Park.swift
+//  lab5
+//
+//  Created by Yuvam Bhargav on 3/23/26.
+//
+
+import Foundation
+
+struct Park: Identifiable, Hashable {
+    let id = UUID()
+    var name: String
+    var location: String
+    var imageName: String
+    var description: String
+}
+
+
+
+
