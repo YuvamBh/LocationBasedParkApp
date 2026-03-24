81b380df369e64f864b77807f88f33b1a93c5707
Yuvam <yuvam05.ed@gmail.com>
2026-03-23 21:47:24 -0700

    viewPageOnMainAlphabeticOrder

--- lab5/ContentView.swift
+++ lab5/ContentView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var parkVM = ParkViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(parkVM.sortedSectionKeys, id: \.self) { key in
+                    if let sectionParks = parkVM.groupedParks[key] {
+                        Section(header: Text(key)) {
+                            ForEach(sectionParks) { park in
+                                NavigationLink(destination: ParkDetailView(park: park)) {
+                                    HStack {
+                                        Image(park.imageName)
+                                            .resizable()
+                                            .frame(width: 60, height: 60)
+                                            .cornerRadius(8)
+                                        
+                                        VStack(alignment: .leading) {
+                                            Text(park.name)
+                                                .font(.headline)
+                                            Text(park.location)
+                                                .font(.subheadline)
+                                                .foregroundColor(.gray)
+                                        }
+                                    }
+                                }
+                            }
+                            .onDelete { offsets in
+                                parkVM.deletePark(at: offsets, in: sectionParks)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Favorite Parks")
+            .toolbar {
+                NavigationLink("Add Park") {
+                    AddParkView(parkVM: parkVM)
+                }
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
