8f1c4d750f1cd405b73601d2c92d771c034f3811
Yuvam <yuvam05.ed@gmail.com>
2026-03-23 20:22:14 -0700

    Initial Commit

---
+++ lab5/lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  lab5App.swift
+//  lab5
+//
+//  Created by Yuvam Bhargav on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
